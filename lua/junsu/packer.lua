87f8665f21d0197d1bf6a3546bc6adedd295adbc
Junsu An <anjjunsu@gmail.com>
2023-09-09 21:35:27 -0700

    Packer on start update

--- lua/junsu/packer.lua
+++ lua/junsu/packer.lua
@@ -12,19 +12,6 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
-
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
@@ -63,4 +50,7 @@ return require('packer').startup(function(use)
 		  {'L3MON4D3/LuaSnip'},     -- Required
 	  }
   }
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
